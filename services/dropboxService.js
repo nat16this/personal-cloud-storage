18caca276b43c16c7a34dad038a10881ab1b5c31
nat16this <nathanomenka555@gmail.com>
2026-08-27 08:26:09 +0100

    Fix duplicate Dropbox service declaration

--- services/dropboxService.js
+++ services/dropboxService.js
@@ -28,34 +28,3 @@ async function getDropboxClient(userId) {
 module.exports = {
   getDropboxClient,
 };
-
-const { Dropbox } = require("dropbox");
-const supabaseAdmin = require("../config/supabaseAdmin");
-
-async function getDropboxClient(userId) {
-  const { data: connection, error } = await supabaseAdmin
-    .from("dropbox_connections")
-    .select("access_token, refresh_token, expires_at")
-    .eq("user_id", userId)
-    .single();
-
-  if (error || !connection) {
-    throw new Error("Dropbox account is not connected.");
-  }
-
-  const dbx = new Dropbox({
-    accessToken: connection.access_token,
-    refreshToken: connection.refresh_token,
-    clientId: process.env.DROPBOX_APP_KEY,
-    clientSecret: process.env.DROPBOX_APP_SECRET,
-  });
-
-  return {
-    dbx,
-    connection,
-  };
-}
-
-module.exports = {
-  getDropboxClient,
-};
